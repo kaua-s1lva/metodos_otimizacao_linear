f0d244f8c2514ebe9e281fb9be3430b736fc1544
Kauã de Souza da Silva <kaua.jesuscristo78@gmail.com>
2025-06-11 17:33:08 -0300

    criar solução gulosa

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #define PESO 1000
 
 int main() {
-    Solucao sol;
+    Solucao sol, sol2;
 
     ler_arquivo("../instancia-toy.txt");
     imprimir_dados_arquivo();
@@ -18,6 +18,12 @@ int main() {
     gerar_solucao(sol);
     calcular_fo(sol);
     imprimir_solucao(sol);
+
+    memcpy(&sol2, &sol, sizeof(sol2));
+    for (int i=0; i<10; i++) {
+        gerar_vizinho(sol2);
+        imprimir_solucao(sol2);
+    }
 
     return 0;
 }
@@ -33,6 +39,51 @@ void gerar_solucao(Solucao& sol) {
 
         sol.vet_aux[vei]++;
     }
+}
+
+void gerar_solucao_aleatoria(Solucao& sol) {
+    memset(&sol.vet_aux, 0, sizeof(sol.vet_aux));
+    memset(&sol.mat_sol, 0, sizeof(sol.mat_sol));
+
+    int vei;
+    for (int i=1; i<=num_cli; i++) {
+        vei = rand() % num_vei;
+        sol.mat_sol[vei][sol.vet_aux[vei]] = i;
+
+        sol.vet_aux[vei]++;
+    }
+}
+
+void gerar_solucao_gulosa(Solucao& sol) {
+    /*
+        IDEIA!!!!!!!!!!
+        Criar uma matriz ordenada para armazenar as posições dos clientes que estão menos longe
+        [
+            [0] => [0, 2, 1, 3]
+            [1] => [0, 1, 3, 2]
+            ...
+        ]
+    */
+
+
+    int j, mat_dis_cpy[MAX_VEI][MAX_CLI];
+    float distancia = mat_dis_cli[0][1];
+
+    memcpy(&mat_dis_cpy, &mat_dis_cli, sizeof(mat_dis_cpy));
+
+    //depósito -> primeiro cliente
+    for (int i=0; i<num_vei; i++) {
+        for (j=1; j<=num_cli; j++) {
+            if (distancia > mat_dis_cpy[0][j] && mat_dis_cpy[0][j] != -1) {
+                distancia = mat_dis_cpy[0][j];
+            }
+        }
+        mat_dis_cpy[0][j] = -1;
+    }
+}
+
+void gerar_solucao_aleatoria_gulosa(Solucao& sol) {
+
 }
 
 void calcular_fo(Solucao& sol) {
@@ -62,7 +113,28 @@ void calcular_fo(Solucao& sol) {
 }
 
 void gerar_vizinho(Solucao& sol) {
-    
+    int vei_pre, vei_pos, pos_cliente, cliente;
+    vei_pre = rand() % num_vei;
+
+    pos_cliente = rand() % sol.vet_aux[vei_pre];
+    cliente = sol.mat_sol[vei_pre][pos_cliente];
+
+    //remover cliente do vei_pre
+    sol.mat_sol[vei_pre][pos_cliente] = 0;
+    while (sol.mat_sol[vei_pre][pos_cliente+1] != 0) {
+        sol.mat_sol[vei_pre][pos_cliente] = sol.mat_sol[vei_pre][pos_cliente+1];
+        sol.mat_sol[vei_pre][pos_cliente+1] = 0;
+        pos_cliente++;
+    }
+    sol.vet_aux[vei_pre]--;
+
+    //inserir cliente no veiculo
+    do {
+        vei_pos = rand() % num_vei;
+    } while (vei_pre == vei_pos);
+
+    sol.mat_sol[vei_pos][sol.vet_aux[vei_pos]] = cliente;
+    sol.vet_aux[vei_pos]++;
 }
 
 void ler_arquivo(char* path) {
@@ -87,6 +159,30 @@ void ler_arquivo(char* path) {
 
     fclose(f);
 }
+
+/*
+void inserir_cliente(Solucao& sol, int& pessoa, int& veiculo) {
+    int i;
+    for (i = sol.vet_aux[veiculo] - 1; i>=0; i--) {
+        if (vet_hora_ini[sol.mat_sol[mot][i]] > vet_hora_ini[tarefa]) {
+            sol.mat_sol[mot][i + 1] = sol.mat_sol[mot][i];
+        } else {
+            break;
+        }
+    }
+
+    sol.mat_sol[mot][i + 1] = tarefa;
+    sol.aux[mot]++;
+}
+
+void remover_cliente(Solucao& sol, int& pos, int& mot) {
+    for (int i=pos; i<sol.aux[mot] - 1; i++) {
+        sol.mat_sol[mot][i] = sol.mat_sol[mot][i+1];
+    }
+
+    sol.aux[mot]--;
+}
+*/
 
 void imprimir_solucao(Solucao& sol) {
     printf("\n-----------------------------------------\n");
