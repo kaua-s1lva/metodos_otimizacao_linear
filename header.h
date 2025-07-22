085ae6c5f9f51468a950f216f85520fa86ab1da8
Kauã de Souza da Silva <kaua.jesuscristo78@gmail.com>
2025-07-21 22:35:41 -0300

    heurística aleatória, primeira melhora e melhor melhora

--- header.h
+++ header.h
@@ -9,13 +9,18 @@ int vet_cap_moc[MAX_MOC];
 
 typedef struct tSolucao {
     int vet_obj[MAX_OBJ];
+    int vet_pes_moc[MAX_OBJ];
     int fo;
 } Solucao;
 
 void ler_arquivo(char* path);
 void imprimir_dados_arquivo();
+void calcular_fo(Solucao& sol);
 void gerar_solucao_aleatoria(Solucao& sol);
 void gerar_solucao_gulosa(Solucao& sol);
 void gerar_solucao_aleatoria_gulosa(Solucao& sol);
 void gerar_vizinho(Solucao& sol);
 void imprimir_solucao(Solucao& sol);
+void heu_BL_rand(Solucao& sol, const int inter);
+void heu_BL_MM(Solucao& sol);
+void heu_BL_PM(Solucao& sol);
--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 #include <time.h>
 #include <string.h>
 
+#define MAX(X,Y) (X > Y ? X : Y)
+
+#define PESO_CAP 100
+
 int main() {
     //srand(time(NULL));
 
@@ -12,15 +16,32 @@ int main() {
     ler_arquivo("../pmm1.txt");
     //imprimir_dados_arquivo();
 
-    gerar_solucao_gulosa(sol);
+    gerar_solucao_aleatoria(sol);
+    calcular_fo(sol);
+    imprimir_solucao(sol);
 
-    memcpy(&sol2, &sol, sizeof(sol2));
-    gerar_vizinho(sol2);
+    heu_BL_PM(sol);
+    calcular_fo(sol);
 
     imprimir_solucao(sol);
-    imprimir_solucao(sol2);
     
     return 0;
+}
+
+void calcular_fo(Solucao& sol)
+{
+    sol.fo = 0;
+    memset(&sol.vet_pes_moc, 0, sizeof(sol.vet_pes_moc));
+    for (int j = 0; j < num_obj; j++)
+    {
+        if (sol.vet_obj[j] != -1)
+        {
+            sol.fo += vet_val_obj[j];
+            sol.vet_pes_moc[sol.vet_obj[j]] += vet_pes_obj[j];
+        }
+    }
+    for (int i = 0; i < num_moc; i++)
+        sol.fo -= PESO_CAP * MAX(0, (sol.vet_pes_moc[i] - vet_cap_moc[i]));
 }
 
 void gerar_vizinho(Solucao& sol) {
@@ -84,11 +105,75 @@ void gerar_solucao_aleatoria_gulosa(Solucao& sol) {
     }
 }
 
+void heu_BL_rand(Solucao& sol, const int inter) {
+    Solucao vizinho;
+    int flag = 1;
+
+    while (flag)
+    {
+        flag = 0;
+        for (int i=0; i<inter; i++) {
+            memcpy(&vizinho, &sol, sizeof(vizinho));
+            gerar_vizinho(vizinho);
+            calcular_fo(vizinho);
+            if (sol.fo <= vizinho.fo) {
+                memcpy(&sol, &vizinho, sizeof(sol));
+                flag = 1;
+            }
+        }
+    }
+}
+
+void heu_BL_MM(Solucao& sol) {
+    int moc_ori, mel_sol = sol.fo, flag = 1;
+
+    while (flag) {
+        flag = 0;
+        for (int i=0; i<num_obj; i++) {
+            for (int j=-1; j<num_moc; j++) {
+                moc_ori = sol.vet_obj[i];
+                sol.vet_obj[i] = j;
+                calcular_fo(sol);
+                if (mel_sol < sol.fo) {
+                    mel_sol = sol.fo;
+                    flag = 1;
+                } else {
+                    sol.vet_obj[i] = moc_ori;
+                }
+            }
+        }
+    }
+}
+
+void heu_BL_PM(Solucao& sol) {
+    int moc_ori, mel_sol = sol.fo, flag = 1;
+
+    while (flag) {
+        flag = 0;
+        INICIO : ;
+        for (int i=0; i<num_obj; i++) {
+            for (int j=-1; j<num_moc; j++) {
+                moc_ori = sol.vet_obj[i];
+                sol.vet_obj[i] = j;
+                calcular_fo(sol);
+                if (mel_sol < sol.fo) {
+                    mel_sol = sol.fo;
+                    flag = 1;
+                    goto INICIO;
+                } else {
+                    sol.vet_obj[i] = moc_ori;
+                }
+            }
+        }
+    }
+}
+
 void imprimir_solucao(Solucao& sol) {
     printf("\n\nVetor solucao: \n");
     for (int i=0; i<num_obj; i++) {
         printf(" %d ", sol.vet_obj[i]);
     }
+    printf("\nFO: %d", sol.fo);
 }
 
 void ler_arquivo(char* path) {
